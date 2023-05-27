7251d2bcf411f2fa721da30f930717224e593473
mario-bruno <106055413+mario-bruno@users.noreply.github.com>
2023-05-28 00:24:33 +0200

    Update in Settings Class

--- src/main/java/com/calendly/calendly/Settings.java
+++ src/main/java/com/calendly/calendly/Settings.java
@@ -5,8 +5,8 @@ public class Settings {
     public final static String[] fonts = {};
     public final static String[] styles = {"css/style.css", "css/dark.css"};
 
-    public final static int DEFAULT_PAGE_HEIGHT = 800;
-    public final static int DEFAULT_PAGE_WIDTH = 600;
+    public final static int DEFAULT_PAGE_HEIGHT = 900;
+    public final static int DEFAULT_PAGE_WIDTH = 1000;
     public final static int DEFAULT_MIN_PAGE_HEIGHT = 600;
     public final static int DEFAULT_MIN_PAGE_WIDTH = 400;
 }
