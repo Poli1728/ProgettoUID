b98bd6cf5400f18f1b4ac7580d8be0c978177fd4
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2023-05-27 23:01:08 +0200

    Aggiunta Controller
    Ho aggiunto i controller e aggiornato le view per i collegamenti

---
+++ src/main/java/com/calendly/calendly/Controller/HomeController.java
@@ -0,0 +1,54 @@
+package com.calendly.calendly.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class HomeController {
+    @FXML
+    private Button ProfiloButton;
+
+    @FXML
+    private Pane ViewPane;
+
+    @FXML
+    void ApriAppuntamenti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApriDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApriEventi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApriImpostazioni(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApriProfili(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApriProfilo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApriStatistiche(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Esci(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/calendly/calendly/Controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.calendly.calendly.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+public class LoginController {
+    @FXML
+    private PasswordField PasswordField;
+
+    @FXML
+    private TextField UsernameField;
+
+    @FXML
+    private Button accediButton;
+
+    @FXML
+    void Accedi(ActionEvent event) {
+        if (UsernameField.getText().equals("Marco") && PasswordField.getText().equals("Marco")){
+
+        }
+    }
+
+}
