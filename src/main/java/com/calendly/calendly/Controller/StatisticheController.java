543f9a5ccee4d9e517fc5e87d639ceef546e4bfa
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2023-05-31 19:52:17 +0200

    Piccoli cambiamenti a statistiche

--- src/main/java/com/calendly/calendly/Controller/StatisticheController.java
+++ src/main/java/com/calendly/calendly/Controller/StatisticheController.java
@@ -2,8 +2,18 @@ package com.calendly.calendly.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
 
 public class StatisticheController {
+
+    @FXML
+    private BarChart<?, ?> chartAnnuale;
+
+    @FXML
+    private BarChart<?, ?> chartMensile;
+
+    @FXML
+    private BarChart<?, ?> chartSettimanale;
 
     @FXML
     void creaPdf(ActionEvent event) {
