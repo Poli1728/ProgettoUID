5e2a6b671414a8543ac79c0d3d4c2db64b40d8e6
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2023-05-31 16:17:42 +0200

    Aggiunta parte degli appuntamenti
    Aggiunta la parte che riguarda gli appuntamenti. Ancora da finire

---
+++ src/main/java/com/calendly/calendly/Controller/AppuntamentiController.java
@@ -0,0 +1,43 @@
+package com.calendly.calendly.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class AppuntamentiController {
+
+    @FXML
+    private TableColumn<?, ?> colonnaDipendente;
+
+    @FXML
+    private TableColumn<?, ?> colonnaEmail;
+
+    @FXML
+    private TableColumn<?, ?> colonnaOrario;
+
+    @FXML
+    private TableColumn<?, ?> colonnaServizio;
+
+    @FXML
+    private TableColumn<?, ?> colonnaUtente;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    void aggiungiAppuntamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modificaAppuntamento(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rimuoviAppuntamento(ActionEvent event) {
+
+    }
+
+}
