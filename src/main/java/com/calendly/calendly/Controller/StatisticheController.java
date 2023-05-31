e3998e509cc9d8114c3315d451244cdb1ab13945
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2023-05-31 19:05:43 +0200

    Aggiunto tasto per salvare le statistiche come pdf

---
+++ src/main/java/com/calendly/calendly/Controller/StatisticheController.java
@@ -0,0 +1,13 @@
+package com.calendly.calendly.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class StatisticheController {
+
+    @FXML
+    void creaPdf(ActionEvent event) {
+
+    }
+
+}
