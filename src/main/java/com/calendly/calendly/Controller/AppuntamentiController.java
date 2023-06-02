716b5a281b99cedac609cc406801dfd361177365
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2023-06-02 11:11:01 +0200

    Aggiunta appuntamenti
    Devo ancora renderli visibili nella tabella

--- src/main/java/com/calendly/calendly/Controller/AppuntamentiController.java
+++ src/main/java/com/calendly/calendly/Controller/AppuntamentiController.java
@@ -1,15 +1,19 @@
 package com.calendly.calendly.Controller;
 
+import com.calendly.calendly.Model.Appuntamento;
 import com.calendly.calendly.SceneHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AppuntamentiController {
+public class AppuntamentiController implements Initializable {
     @FXML
     private Pane pane;
     @FXML
@@ -23,9 +27,6 @@ public class AppuntamentiController {
 
     @FXML
     private TableColumn<?, ?> colonnaServizio;
-
-    @FXML
-    private TableColumn<?, ?> colonnaUtente;
 
     @FXML
     private TableView<?> table;
@@ -52,8 +53,9 @@ public class AppuntamentiController {
         modificaPane("Rimuovi");
     }
 
-    void initialize(){
-        //Qui va l'inserimento nella tabella
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Appuntamento app = new Appuntamento("marco.martino.kr@gmail.com", "10:30 02/03/2023", "Mario Bruno", "Taglio capelli");
+        
     }
-
 }
--- src/main/java/com/calendly/calendly/Model/Appuntamento.java
+++ src/main/java/com/calendly/calendly/Model/Appuntamento.java
@@ -1,5 +1,47 @@
 package com.calendly.calendly.Model;
 
 public class Appuntamento {
-    String email;
+    private String email;
+    private String orario;
+    private String dipendente;
+    private String servizio;
+
+    public Appuntamento(String e, String o, String d, String s){
+        email = e;
+        orario = o;
+        dipendente = d;
+        servizio = s;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrario() {
+        return orario;
+    }
+
+    public void setOrario(String orario) {
+        this.orario = orario;
+    }
+
+    public String getDipendente() {
+        return dipendente;
+    }
+
+    public void setDipendente(String dipendente) {
+        this.dipendente = dipendente;
+    }
+
+    public String getServizio() {
+        return servizio;
+    }
+
+    public void setServizio(String servizio) {
+        this.servizio = servizio;
+    }
 }
