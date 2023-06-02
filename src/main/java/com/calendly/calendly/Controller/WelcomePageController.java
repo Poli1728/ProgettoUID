d46d69a1320ed5d0444ac288286f77fcab3c1860
mario-bruno <106055413+mario-bruno@users.noreply.github.com>
2023-06-02 21:20:58 +0200

    Aggiunta immagini e animazione a immagini in WelcomePageController.java

--- src/main/java/com/calendly/calendly/Controller/WelcomePageController.java
+++ src/main/java/com/calendly/calendly/Controller/WelcomePageController.java
@@ -1,17 +1,21 @@
 package com.calendly.calendly.Controller;
+import com.calendly.calendly.Main;
 import com.calendly.calendly.SceneHandler;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
@@ -28,11 +32,15 @@ public class WelcomePageController {
 
     @FXML
     void actionContinueButton(ActionEvent event) {
-        //TODO quando premuto, passa alla schermata di creazione dell'account proprietario + animazione
+        if (timeline != null)
+            timeline.stop();
         SceneHandler.getInstance().launchLogin();
+
     }
 
-    private List<Image> images;
+
+    private ImageView imageView = new ImageView();
+    private List<Image> images = new LinkedList<>();
 
     //--------------------------
     private Stage stage;
@@ -66,8 +74,8 @@ public class WelcomePageController {
         double width  = stackPane.getWidth();
         double height = stackPane.getHeight();
 
-        double x = pane.getWidth()/2 - width/2;
-        double y = pane.getHeight()/2 - height/2;
+        double x = (pane.getWidth() - width) / 2;
+        double y = (pane.getHeight() - height) / 2;
 
         stackPane.resizeRelocate(x, y, width, height);
     }
@@ -79,17 +87,65 @@ public class WelcomePageController {
     }
 
 
+
+    private Timeline timeline;
+    private int indexImage = 0;
+
+
     private void setImages() {
         int N_IMAGES = 4;
 
-        try {
-            for (int i = 1; i <= N_IMAGES; i++) {
-                Image image = new Image(WelcomePageController.class.getResourceAsStream("/img/welcome1.png"));
+        for (int i = 1; i <= N_IMAGES; i++) {
+            try {
+                Image image = new Image(Objects.requireNonNull(Main.class.getResourceAsStream("img/welcome" + i + ".png")));
                 images.add(image);
+            } catch (Exception e) {
+                System.out.println(i + " " + e.toString());
             }
-        } catch (Exception e) {
-            System.out.println(e.toString());
         }
+
+        int DEFAULT_IMAGE = 2;
+
+        imageView.maxWidth(stackPane.getMaxWidth());
+        imageView.maxHeight(210);
+
+
+
+        Platform.runLater(() -> {
+            timeline = new Timeline(new KeyFrame(Duration.seconds(2), e -> setLayoutImageView()));
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+        });
+
+
+        stackPane.layoutBoundsProperty().addListener(observable -> {
+            setLayoutImageView();
+        });
+
+
+
+    }
+
+    private void setLayoutImageView() {
+
+        imageView.setImage(images.get(indexImage));
+
+
+        imageView.setFitWidth(images.get(indexImage).getWidth() * 0.5);
+        imageView.setFitHeight(images.get(indexImage).getHeight() * 0.4);
+
+        imageView.setLayoutX(stackPane.getWidth()/2 - imageView.getFitWidth()/2);
+        imageView.setLayoutY(0);
+
+
+        if (!stackPane.getChildren().contains(imageView)) {
+            stackPane.getChildren().add(imageView);
+        }
+
+        indexImage++;
+
+        if (indexImage > images.size()-1)
+            indexImage = 0;
 
     }
 
