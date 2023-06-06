18de71843a6c4f909ad422457477aaec3e2f0a50
Marco Martino <59832916+Marco-Maker@users.noreply.github.com>
2023-06-06 16:23:17 +0200

    Appuntamento update

--- src/main/java/com/calendly/calendly/Controller/AppuntamentiController.java
+++ src/main/java/com/calendly/calendly/Controller/AppuntamentiController.java
@@ -2,19 +2,16 @@ package com.calendly.calendly.Controller;
 
 import com.calendly.calendly.Model.Appuntamento;
 import com.calendly.calendly.Model.GestoreAppuntamenti;
-import com.calendly.calendly.SceneHandler;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -34,16 +31,17 @@ public class AppuntamentiController implements Initializable {
     private TableColumn<Appuntamento, String> colonnaDipendente;
 
     @FXML
-    private TableColumn<Appuntamento, String> colonnaEmail;
+    private TableColumn<Appuntamento, Integer> colonnaId;
 
     @FXML
-    private TableColumn<Appuntamento, Integer> colonnaId;
+    private TableColumn<Appuntamento, String> colonnaEmail;
 
     @FXML
     private TableColumn<Appuntamento, String> colonnaNomeCognome;
 
     @FXML
     private TableColumn<Appuntamento, String> colonnaNumero;
+    @FXML
     private TableColumn<Appuntamento, Double> colonnaPrezzo;
 
     @FXML
@@ -54,6 +52,28 @@ public class AppuntamentiController implements Initializable {
 
     @FXML
     private ComboBox<String> filtroBox;
+
+    private void setCellValue(){
+        colonnaId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colonnaEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colonnaData.setCellValueFactory(new PropertyValueFactory<>("data"));
+        colonnaPrezzo.setCellValueFactory(new PropertyValueFactory<>("prezzo"));
+        colonnaDipendente.setCellValueFactory(new PropertyValueFactory<>("dipendente"));
+        colonnaNomeCognome.setCellValueFactory(new PropertyValueFactory<>("identificativo"));
+        colonnaNumero.setCellValueFactory(new PropertyValueFactory<>("numero"));
+        colonnaServizio.setCellValueFactory(new PropertyValueFactory<>("servizio"));
+    }
+
+    private void aggiungiItems(){
+        filtroBox.getItems().add("Id");
+        filtroBox.getItems().add("Email");
+        filtroBox.getItems().add("Nome");
+        filtroBox.getItems().add("Cognome");
+        filtroBox.getItems().add("Numero");
+        filtroBox.getItems().add("Data");
+        filtroBox.getItems().add("Dipendente");
+        filtroBox.getItems().add("Servizio");
+    }
 
     @FXML
     void aggiungiAppuntamento(ActionEvent event) throws IOException {
@@ -74,27 +94,12 @@ public class AppuntamentiController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //aggiungiItems();
-        filtroBox.getItems().add("Id");
-        filtroBox.getItems().add("Email");
-        filtroBox.getItems().add("Nome");
-        filtroBox.getItems().add("Cognome");
-        filtroBox.getItems().add("Numero");
-        filtroBox.getItems().add("Data");
-        filtroBox.getItems().add("Dipendente");
-        filtroBox.getItems().add("Servizio");
-
-        colonnaId.setCellValueFactory(new PropertyValueFactory<>("id"));
-
+        aggiungiItems();
         try {
             ArrayList<Appuntamento> app = GestoreAppuntamenti.getInstance().listaAppuntamenti();
-            for (Appuntamento i : app){
-                System.out.println(i.getIdentificativo());
-            }
             ObservableList<Appuntamento> observableApp = FXCollections.observableArrayList(app);
             table.setItems(observableApp);
-            //colonnaEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
-
+            setCellValue();
 
         } catch (SQLException e) {}
     }
